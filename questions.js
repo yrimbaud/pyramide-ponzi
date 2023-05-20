5fd4babd808dd9879ed0dc9d1f28121105f47c69
Yann Rimbaud <yrimbaud@gmail.com>
2023-05-20 11:12:47 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,29 @@
+function detectPonzi() {
+    let investmentReturns = document.getElementById("investmentReturns").checked;
+    let recruitment = document.getElementById("recruitment").checked;
+    let returnsConsistency = document.getElementById("returnsConsistency").checked;
+    let realProduct = document.getElementById("realProduct").checked;
+    let registered = document.getElementById("registered").checked;
+    let transparent = document.getElementById("transparent").checked;
+    let withdrawalPenalty = document.getElementById("withdrawalPenalty").checked;
+    let aggressiveSales = document.getElementById("aggressiveSales").checked;
+    let reinvestment = document.getElementById("reinvestment").checked;
+    let hardToUnderstand = document.getElementById("hardToUnderstand").checked;
+
+    if (
+        investmentReturns &&
+        recruitment &&
+        returnsConsistency &&
+        !realProduct &&
+        !registered &&
+        !transparent &&
+        withdrawalPenalty &&
+        aggressiveSales &&
+        reinvestment &&
+        hardToUnderstand
+    ) {
+        document.getElementById("result").innerHTML = "Il est possible que l'offre soit une pyramide de Ponzi.";
+    } else {
+        document.getElementById("result").innerHTML = "En se basant sur vos réponses, il semblerait que l'offre ne soit pas une pyramide de Ponzi.";
+    }
+}
